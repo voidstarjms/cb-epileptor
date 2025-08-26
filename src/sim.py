8808a11a3035c38fccbfbb4743fb433999ff486f
Jackson Sweet <sweetj4@wwu.edu>
2025-08-26 16:42:06 -0700

    Various changes, still broken

--- src/sim.py
+++ src/sim.py
@@ -3,21 +3,18 @@ from matplotlib import pyplot as plt
 import argparse
 
 def main():
-    sim_duration = 10 * second
+    sim_duration = 120 * second
 
-    Wmax = 2.5 * mvolt
-    '''W = TimedArray((2 * Wmax * np.random.rand(sim_duration * \
-                    int(ceil(1 / defaultclock.dt))) \
-                   - Wmax) * second, ceil(1 / defaultclock.dt))'''
-    W = TimedArray(np.zeros(10 * 10000), defaultclock.dt)
+    # Divide by 1000 to keep unitless without using milli prefix
+    Wmax = 0 / 1000000
 
-    tau = 1 * second
+    tau = 1 * msecond
 
     pop1_eqs = '''
     dx/dt = (y - a * x ** 3 + b * x ** 2 - z + I_app + C_E * (x_bar - x) +
-        sigma * I_syn_intra + sigma * I_syn_inter + W(t)) / tau : 1
+        sigma * I_syn_intra / amp + sigma * I_syn_inter / amp + xi * Wmax * sqrt(second)) / tau : 1
     dy/dt = (c - d * x ** 2 - y) / tau : 1
-    dz/dt = r * (s * (x + x2_bar - x_naught)) / tau : 1
+    dz/dt = r * (s * (x + x2_bar - x_naught)) / tau : 1    
 
     a : 1
     b : 1
@@ -26,21 +23,24 @@ def main():
     s : 1
     r : 1
     x_naught : 1
-    I_syn_intra : 1
-    I_syn_inter : 1
+    I_syn_intra : amp
+    I_syn_inter : amp
     I_app : 1
     C_E : 1
     sigma : 1
 
-    x_bar : 1 (shared)
+    x_contrib : 1 (linked)
+    x_total = x_contrib : 1 (shared)
+    x_bar = x_total / N : 1 (shared)
+
     # This needs to be passed in throughout runtime
     x2_bar : 1 (shared)
     '''
 
     pop2_eqs = '''
     dv/dt = (I_app - gL * (v - E_L) - gK * n * (v - E_K) - gCa * m_inf *
-        (v - E_Ca) + sigma * C_E * (x_bar - x) * amp + I_syn_inter * amp +
-        I_syn_intra * amp - sigma * 0.3 * (z_bar - 3) * amp + sigma * W(t)
+        (v - E_Ca) + sigma * C_E * (x_bar - x) * amp + I_syn_inter +
+        I_syn_intra - sigma * 0.3 * (z_bar - 3) * amp + sigma * xi * Wmax * sqrt(second)
         * amp) / Cm : volt
     dn/dt = phi * (n_inf - n) / tau_n : 1
 
@@ -52,8 +52,8 @@ def main():
 
     Cm : farad
     I_app : amp
-    I_syn_inter : 1
-    I_syn_intra : 1
+    I_syn_inter : amp
+    I_syn_intra : amp
     v1 : volt
     v2 : volt
     v3 : volt
@@ -69,14 +69,16 @@ def main():
     sigma : 1
     z_bar : 1
 
-    x_bar : 1 (shared)
+    x_contrib : 1 (linked)
+    x_total = x_contrib : 1 (shared)
+    x_bar = x_total / N : 1 (shared)
 
     x2_bar : 1 (shared)
     '''
 
-    N1 = NeuronGroup(10, pop1_eqs, method='rk4')
+    N1 = NeuronGroup(10, pop1_eqs, method='euler')
 
-    #N1.x_contrib = linked_var(N1, 'x')
+    N1.x_contrib = linked_var(N1, 'x')
 
     N1.a = 1
     N1.b = 3
@@ -89,61 +91,63 @@ def main():
     N1.C_E = 1
     N1.sigma = 1/50
 
-    N2 = NeuronGroup(10, pop2_eqs, method='rk4')
+    N2 = NeuronGroup(10, pop2_eqs, method='euler')
 
-    #N2.x_contrib = linked_var(N2, 'x')
+    N2.x_contrib = linked_var(N2, 'x')
 
     N2.Cm = 20 * ufarad
-    N2.I_app = 40 * uamp
-    N2.v1 = 1.2 * mvolt
-    N2.v2 = 18 * mvolt
-    N2.v3 = 12 * mvolt
-    N2.v4 = 17.4 * mvolt
+    N2.I_app = 400 * pamp
+    N2.v1 = 10 * mvolt
+    N2.v2 = 15 * mvolt
+    N2.v3 = -1 * mvolt
+    N2.v4 = 14.5 * mvolt
     N2.phi = 0.067 * Hz
     N2.E_Ca = 120 * mvolt
     N2.E_K = -84 * mvolt
     N2.E_L = -60 * mvolt
-    N2.gL = 4 * nsiemens
-    N2.gCa = 8 * nsiemens
-    N2.gK = 11.2 * nsiemens
+    N2.gL = 2 * msiemens
+    N2.gCa = 4 * msiemens
+    N2.gK = 8 * msiemens
 
     universal_syn_eqs ='''
-    du/dt = (alpha * T * (1 - u) - beta * u) / tau : 1 (clock-driven)
-    T = Tmax / (1 + exp(-(x_pre - Vt) / Kp)) : 1
+    du/dt = (alpha * T * (1 - u) - beta * u) : 1 (clock-driven)
+    T = Tmax / (1 + exp(-(x_pre * mvolt - Vt) / Kp)) : mM
 
-    alpha : 1
-    beta : 1
+    alpha : mmolar ** -1 * second ** -1
+    beta : second ** -1
     '''
 
     syn_intra_eqs ='''
-    I_syn_intra_post = (-G * u * (x_pre - E)) : 1 (summed)
+    I_syn_intra_post = (-G * u * (x_pre * mvolt - E)) : amp (summed)
 
-    G : 1
-    E : 1
+    G : siemens
+    E : volt
     ''' + universal_syn_eqs
 
     syn_inter_eqs ='''
-    I_syn_inter_post = (-G * u * (x_pre - E)) : 1 (summed)
+    I_syn_inter_post = (-G * u * (x_pre * mvolt - E)) : amp (summed)
 
     #x2_bar_post = x_bar_pre : 1 (summed)
     
-    G : 1
-    E : 1
+    G : siemens
+    E : volt
     ''' + universal_syn_eqs
 
     # Shared synapse parameters
-    Tmax = 1
-    Vt = 2
-    Kp = 5
+    Tmax = 1 * mM
+    Vt = 2 * mV
+    Kp = 5 * mV
 
     # Synapse coupling parameters
-    G_intra = [0.4, 0.4]
-    G_inter = [0.4, 0.4]
+    # 1 -> 1, 2 -> 2
+    G_intra = [0, 0] * usiemens
+    # 1 -> 2, 2 -> 1
+    G_inter = [0, 0] * usiemens
 
     # Excitatory synapse parameters
-    alpha_exc = 1.1
-    beta_exc = 0.19
-    E_exc = 0
+    alpha_exc = 1.1 / mM / msecond
+    beta_exc = 0.19 / msecond
+    E_exc = 0 * mvolt
 
     # Intrapopulation excitatory synapses
     S_exc_intra = Synapses(N1, N1, syn_intra_eqs, method='rk4')
@@ -162,9 +166,9 @@ def main():
     S_exc_inter.G = G_inter[0]
 
     # Inhibitory synapse parameters
-    alpha_inh = 5
-    beta_inh = 0.18
-    E_inh = -80
+    alpha_inh = 5 / mM / msecond
+    beta_inh = 0.18 / msecond
+    E_inh = -80 * mvolt
 
     # Intrapopulation excitatory synapses
     S_inh_intra = Synapses(N2, N2, syn_intra_eqs, method='rk4')
@@ -183,13 +187,15 @@ def main():
     S_inh_inter.G = G_inter[1]
 
     # State monitors
-    M_N1 = StateMonitor(N1, 'z', record=True)
-    M_N2 = StateMonitor(N2, 'v', record=True)
+    M_N1 = StateMonitor(N1, ['x', 'y', 'z', 'x_bar'], record=True)
+    M_N2 = StateMonitor(N2, ['v', 'x', 'x_bar'], record=True)
 
-    run(10 * second)
+    run(sim_duration)
 
-    plt.plot(M_N2.t, M_N2.v[0], label="Population 1")
-    print(M_N2.v[0][0:10])
+    plt.plot(M_N1.t, M_N2.v[0], label="Population 2")
+    #print(M_N1.x[0][0:10])
+    plt.xlabel("Time (s)")
+    plt.ylabel("v")
     plt.show()
 
 if __name__ == "__main__":
