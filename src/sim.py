c4f0a49152f271791450b60efac472a0510028fa
Jade Sweet <voidstarjms@gmail.com>
2025-10-15 13:08:42 -0700

    Fixed equations for synapses, added plotting

--- src/sim.py
+++ src/sim.py
@@ -1,15 +1,22 @@
 from brian2 import *
 from matplotlib import pyplot as plt
+import numpy as np
+import scipy
+from scipy import signal
 import argparse
 
-def main():
-    sim_duration = 0.2 * second
+def run_sim():
+    sim_duration = 60 * second
     tau = 1 * msecond
-    defaultclock.dt = tau/200
+    defaultclock.dt = tau / 20
 
     num_cells = 40
 
-    Wmax = 0#20
+    # Discrepancy in paper regarding Wmax
+    # Table shows [1, 20] with no units, while text states [2.5, 20] * mV
+    Wmax = 0.0025
+
+    coupling = 0.2
 
     ### Population 1 (Hindmarsh-Rose)
 
@@ -21,14 +28,16 @@ def main():
     d = 5
     s = 8
     I_app_1 = 3.1
-    x_naught = -4.5
-    r = 0.00002
-
+    x_naught = -2.5
+    r = 0.000004
+    sigma_1 = 1/50
+    
     # Population 1 equations
     pop1_eqs = '''
-    dx/dt = (y - a * x ** 3 + b * x ** 2 - z + I_app_1 + sigma_1 * (x_bar - x)
-        + sigma_1 * (2 * Wmax * xi * sqrt(second) - Wmax)
-        + sigma_1 * I_syn_intra / amp + sigma_1 * I_syn_inter / amp) / tau : 1
+    dx/dt = (y - a * x ** 3 + b * x ** 2 - z + I_app_1
+        + sigma_1 * (coupling * (x_bar - x)
+        + 2 * Wmax * xi * sqrt(second) - Wmax
+        + (I_syn_intra + I_syn_inter) / amp)) / tau : 1
     dy/dt = (c - d * x ** 2 - y) / tau : 1
     dz/dt = r * (s * (x + x2_bar - x_naught) - z_bar) / tau : 1
 
@@ -39,20 +48,15 @@ def main():
     I_syn_inter : amp
     '''
 
-    N1 = NeuronGroup(num_cells, pop1_eqs, method='euler') 
+    N1 = NeuronGroup(num_cells, pop1_eqs, method='euler', threshold='x > 0', reset='')
     N1.x = x_naught
     N1.y = 'c - d*x**2'
     N1.z = 'r*(s*(x - x_naught))'
 
     # Population 1 gap junctions
-    sigma_1 = 1/50
     gap_junction_exc_eqs ='''
-    x_bar_post = x / num_cells : 1 (summed)
-    z_bar_post = z / num_cells : 1 (summed)
-    '''
-
-    gap_junction_inh_eqs ='''
-    x_bar_post = x / num_cells : 1 (summed)
+    x_bar_post = x_pre / num_cells : 1 (summed)
+    z_bar_post = z_pre / num_cells : 1 (summed)
     '''
 
     gap_junctions_1 = Synapses(N1, N1, gap_junction_exc_eqs)
@@ -74,13 +78,13 @@ def main():
     gL = 2 * msiemens
     gCa = 4 * msiemens
     gK = 8 * msiemens
-
+    sigma_2 = 50 * uA
+    
     # Population 2 equations    
     pop2_eqs = '''
     dv/dt = (I_app_2 - gL*(v-E_L) - gK*n*(v-E_K) - gCa*m_inf*(v-E_Ca)
-        + sigma_2 * (2 * Wmax * xi * sqrt(second) - Wmax) +
-        sigma_2 * (x_bar - x) + I_syn_inter + I_syn_intra
-        - sigma_2 * 0.3 * (z_bar - 3)) / Cm : volt
+        + sigma_2 * (2 * Wmax * xi * sqrt(second) - Wmax +
+        coupling * (x_bar - x) - 0.3 * (z_bar - 3)) + I_syn_inter + I_syn_intra) / Cm : volt
     dn/dt = phi * (n_inf - n) / tau_n : 1
 
     m_inf = 0.5 * (1 + tanh((v - v1) / v2)) : 1
@@ -95,42 +99,35 @@ def main():
     I_syn_intra : amp
     '''
     
-    N2 = NeuronGroup(num_cells, pop2_eqs, method='euler')
+    N2 = NeuronGroup(num_cells, pop2_eqs, method='euler', threshold='v > 0 * volt', reset='')
     N2.v = E_L
     N2.n = 'n_inf'
     
     # Population 2 gap junctions
-    sigma_2 = 50 * uA
+    gap_junction_inh_eqs ='''
+    x_bar_post = x_pre / num_cells : 1 (summed)
+    '''
+
     gap_junctions_2 = Synapses(N2, N2, gap_junction_inh_eqs)
     gap_junctions_2.connect()
 
-    universal_syn_eqs ='''
+    syn_eqs ='''
     du/dt = (alpha * T * (1 - u) - beta * u) : 1 (clock-driven)
-    T = Tmax / (1 + exp(-(x_pre * mvolt - Vt) / Kp)) : mM
+    T = Tmax / (1 + exp(-(x_bar_pre * mvolt - Vt) / Kp)) : mM
 
+    G : siemens
+    E : volt
     alpha : mmolar ** -1 * second ** -1
     beta : second ** -1
     '''
 
-    syn_intra_eqs ='''
+    intra_syn_eqs = '''
     I_syn_intra_post = (-G * u * (x_post * mvolt - E)) : amp (summed)
+    ''' + syn_eqs
 
-    G : siemens
-    E : volt
-    ''' + universal_syn_eqs
-
-    syn_inter_inh_eqs ='''
+    inter_syn_eqs = '''
     I_syn_inter_post = (-G * u * (x_post * mvolt - E)) : amp (summed)
-
-    #x2_bar_post = x_bar_pre : 1
-
-    G : siemens
-    E : volt
-    ''' + universal_syn_eqs
-
-    syn_inter_exc_eqs ='''
-    #z_bar_post = z_bar_pre : 1
-    ''' + syn_inter_inh_eqs
+    ''' + syn_eqs
 
     # Synapse parameters
     Vt = 2 * mV
@@ -142,53 +139,107 @@ def main():
     beta_inh = 0.18 / msecond
     Esyn_exc = 0 * mV
     Esyn_inh = -80 * mV
+    G_intra = 0.1 * uS
+    G_inter = 0.2 * uS
 
     # Population 1 synapses to self
-    S1_to_1 = Synapses(N1, N1, syn_intra_eqs)
+    S1_to_1 = Synapses(N1, N1, intra_syn_eqs, method='euler')
     S1_to_1.connect()
     S1_to_1.E = Esyn_exc
     S1_to_1.alpha = alpha_exc
     S1_to_1.beta = beta_exc
+    S1_to_1.G = G_intra
 
     # Population 1 synapses to pop 2
-    S1_to_2 = Synapses(N1, N2, syn_inter_exc_eqs)
+    S1_to_2 = Synapses(N1, N2, inter_syn_eqs, method='euler')
     S1_to_2.connect()
     S1_to_2.run_regularly('x2_bar_post = x_bar_pre', dt=defaultclock.dt)
     S1_to_2.run_regularly('z_bar_post = z_bar_pre', dt=defaultclock.dt)
     S1_to_2.E = Esyn_exc
     S1_to_2.alpha = alpha_exc
     S1_to_2.beta = beta_exc
+    S1_to_2.G = G_inter
 
     # Population 2 synapses to self
-    S2_to_2 = Synapses(N2, N2, syn_intra_eqs)
+    S2_to_2 = Synapses(N2, N2, intra_syn_eqs, method='euler')
     S2_to_2.connect()
     S2_to_2.E = Esyn_inh
     S2_to_2.alpha = alpha_inh
     S2_to_2.beta = beta_inh
+    S2_to_2.G = G_intra
 
     # Population 2 synapses to pop 1
-    S2_to_1 = Synapses(N2, N1, syn_inter_inh_eqs)
+    S2_to_1 = Synapses(N2, N1, inter_syn_eqs, method='euler')
     S2_to_1.connect()
-    #S2_to_1.run_regularly()
     S2_to_1.E = Esyn_inh
     S2_to_1.alpha = alpha_inh
     S2_to_1.beta = beta_inh
+    S2_to_1.G = G_inter
+
+    run(1 * second)
 
     # Neuron group state monitors
     M_N1 = StateMonitor(N1, ['x', 'y', 'z'], record=True)
-    M_N2 = StateMonitor(N2, 'x', record=True)
+    M_N2 = StateMonitor(N2, ['x', 'v'], record=True)
+
+    SM_N1 = SpikeMonitor(N1)
+    SM_N2 = SpikeMonitor(N2)
     
     run(sim_duration)
+    
+    t = np.asarray(M_N1.t)
+    x1 = np.asarray(M_N1.x)
+    y1 = np.asarray(M_N1.y)
+    z1 = np.asarray(M_N1.z)
+    x2 = np.asarray(M_N2.x)
+    v2 = np.asarray(M_N2.v)
 
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
-    ax1.plot(M_N1.t, M_N1.x[0])
-    ax1.set_xlabel("Time (s)")
-    ax1.set_ylabel("x1")
-    ax2.plot(M_N2.t, M_N2.x[0])
-    ax2.set_xlabel("Time (s)")
-    ax2.set_ylabel("x2")
-    #plt.savefig("figures/max_noise_60s.png", format="png")
-    plt.show()
+    np.savez("output_data.npz", t=t, x1=x1, y1=y1, z1=z1, x2=x2, v2=v2)
+
+def plot_output():
+    arrs = np.load("output_data.npz")
+
+    t = arrs['t']
+    x1 = arrs['x1']
+    y1 = arrs['y1']
+    z1 = arrs['z1']
+    x2 = arrs['x2']
+    v2 = arrs['v2']
+
+    # fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+    # ax1.plot(t, x1[0])
+    # ax1.set_xlabel("Time (s)")
+    # ax1.set_ylabel("x1")
+    # ax2.plot(t, x2[0])
+    # ax2.set_xlabel("Time (s)")
+    # ax2.set_ylabel("x2")
+
+    mean_potential = 0.8 * np.mean(x1, axis=0) + 0.2 * np.mean(x2, axis=0)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 8))
+    ax1.plot(t, mean_potential)
+    
+    fs = 1 / defaultclock.dt / Hz
+    f, Pxx = signal.welch(mean_potential, fs=fs)
+    ax2.plot(f, Pxx)
+    plt.savefig("figures/interictal_power_spectrum.png", format="png")
+
+    fig = plt.figure()
+    f, ts, Sxx = scipy.signal.spectrogram(mean_potential, fs)
+    fig = plt.pcolormesh(ts, f, Sxx, shading='gouraud')
+
+    plt.savefig("figures/interictal_spectrogram.png", format="png")
+
+def main():
+    ### Run mode string
+    # r - run simulation
+    # s - save simulation results
+    # p - plot results
+    run_mode = 'rp'
+
+    if ('r' in run_mode):
+        run_sim()
+    if ('p' in run_mode):
+        plot_output()
 
 if __name__ == "__main__":
     main()
