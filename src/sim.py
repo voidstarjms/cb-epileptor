3faa88240944714deaf5ea3b0242167c5138c143
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2026-03-04 16:56:18 -0800

    Removing G from base sim.py

--- src/sim.py
+++ src/sim.py
@@ -24,19 +24,11 @@ coupling_vals = [0, 0, 0.2, 0.09, 0]
 # x_naught_vals = [-4.5]
 # coupling_vals = [0.09]
 
-G_inter_vals = [0.1, 0.1, 0.2, 0.2, 0.1] * uS
-G_intra_vals = [0.1, 0.2, 0.2, 0.1, 0.1] * uS
-
 x_naught_dt = params.SIM_DURATION//len(x_naught_vals)
 coupling_dt = params.SIM_DURATION//len(coupling_vals)
 
-G_inter_dt = params.SIM_DURATION//len(G_inter_vals)
-G_intra_dt = params.SIM_DURATION//len(G_intra_vals)
-
 timed_x_naught = TimedArray(x_naught_vals, dt=x_naught_dt)
 timed_coupling_strength = TimedArray(coupling_vals, dt=coupling_dt)
-
-timed_G_inter = TimedArray(G_inter_vals, dt=G_inter_dt)
 
 def run_sim():
     # Setup Simulation
