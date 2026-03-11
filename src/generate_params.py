6682aaecc52b8174101a89084473dba0024b9893
pranav-cheraku <pranavcheraku@gmail.com>
2026-03-11 13:48:27 -0700

    Updated X0 and CE values

--- src/generate_params.py
+++ src/generate_params.py
@@ -4,8 +4,8 @@ Run once before submitting: python generate_params.py
 """
 import numpy as np
 
-param1_values = np.linspace(0.0, 0.5, 8)    # CE (coupling strength)
-param2_values = np.linspace(-4.5, -2.5, 8)  # X0 (epileptogenicity)
+param1_values = np.linspace(0.0, 0.2, 8)     # CE (coupling strength)
+param2_values = np.linspace(-4.5, -3.5, 8)  # X0 (epileptogenicity)
 
 lines = []
 for x0 in param2_values:
