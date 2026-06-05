cba529f6a2546e2e88bd81d73477edf72d6f1778
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2026-06-05 16:14:36 -0700

    feat: update saved pkl members

--- src/sweep/run_single_sim.py
+++ src/sweep/run_single_sim.py
@@ -134,8 +134,6 @@ def main():
         'r_inh':            float(r_inh),
         'exc_spike_mat':    spike_matrix_1.astype(np.uint8),
         'inh_spike_mat':    spike_matrix_2.astype(np.uint8)
-
-
     }
     with open(os.path.join(results_dir, f'metrics_{job_id}.pkl'), 'wb') as f:
         pickle.dump(job_result, f)
